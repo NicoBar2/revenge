444f62f373b786ee612eea14ba063a3421d3b917
NicoBar2 <117194905+NicoBar2@users.noreply.github.com>
2025-01-28 20:52:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showSurprise() {
+    const surprise = document.querySelector('.surprise');
+    surprise.classList.remove('hidden');
+    surprise.style.animation = 'fadeIn 1s ease-in-out';
+  }
+  
